6401cdc37f58aa8718793dd7cb9bf4d3a4b690a4
i1123581321 <gokinjolno112358@gmail.com>
2022-04-30 16:28:15 +0800

    add split by same length

--- src/text_split/split.py
+++ src/text_split/split.py
@@ -5,28 +5,36 @@ parser = argparse.ArgumentParser(description="a simple parser")
 
 parser.add_argument("filename", type=str)
 parser.add_argument("lineno", nargs="+", type=int)
+parser.add_argument("--same_length", action=argparse.BooleanOptionalAction)
 
 
 def main():
     args = parser.parse_args()
     filename = args.filename
     linenos = args.lineno
+    same_length = args.same_length
     linenos = list(map(lambda x: x - 1, linenos))
     linenos.sort()
     results = []
-
     with open(filename, "r", encoding="utf-8") as f:
         content = f.readlines()
+        if not same_length:
+            start = 0
 
-    start = 0
+            for lineno in linenos:
+                results.append("".join(content[start:lineno]))
+                start = lineno
+            results.append("".join(content[start:]))
 
-    for lineno in linenos:
-        results.append("".join(content[start:lineno]))
-        start = lineno
-    results.append("".join(content[start:]))
+        else:
+            lineno = linenos[0] + 1 if linenos[0] else 100000
+            start = 0
+            while start < len(content):
+                results.append("".join(content[start: start + lineno]))
+                start += lineno
 
-    name, ext = os.path.splitext(filename)
+        name, ext = os.path.splitext(filename)
 
-    for i, result in enumerate(results):
-        with open(f"{name}-{i + 1:02}{ext}", "w", encoding="utf-8") as f:
-            f.write(result)
+        for i, result in enumerate(results):
+            with open(f"{name}-{i + 1:02}{ext}", "w", encoding="utf-8") as f:
+                f.write(result)
